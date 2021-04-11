2f9d15d4ee07a65432e3dc28c296cc212cbc289c
Kratos-28 <surajarya517@gmail.com>
2021-04-11 17:48:19 +0530

    Creating file and sorting with selection sort

--- Implemention/run.c
+++ Implemention/run.c
@@ -1,12 +1,71 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <time.h>
+#include "sort.h"
+
+
+void file_creating()
+{
+	// This program will create same sequence of
+	// random numbers on every program run
+	FILE* fptr;
+
+	// creating a file "random.txt" in "write" mode
+	fptr = fopen("random.txt", "w");
+	int i;
+	if (fptr == NULL) {
+		printf("ERROR");
+		exit(1);
+	}
+
+	for (i = 0; i < 10000; i++) {
+
+		// to generate number less than 100000
+		int val = rand() % 100000;
+
+		// storing data to file
+		fprintf(fptr, "%d ", val);
+	}
+
+	// closing the file
+	fclose(fptr);
+	printf("numbers generated successfully !! ");
+	
+}
+
+
 
 int main()
-{   int array[]={54,30,45,31,22};
-    int size=sizeof(array)/sizeof(array[0]);
-    double Execution_Time_For_Bubble_Sort=bubble_sort(array,size);
-    printf("Bubble sort is %f\n",Execution_Time_For_Bubble_Sort);
+{   //Creating random integers and storing into File 
+    file_creating();
 
-    return 0;
+    //TO Store the integer number from file to an array
+    int i=0,n=0;
+	
+    // declaring array to store data from file
+	int arr[100000];
+    
+    // declaring file pointer
+	FILE* fptr;
+
+	// opening the integer file.
+	fptr = fopen("random.txt", "r");
+
+	// scanning integer from file to array
+	while (fscanf(fptr, "%d", &arr[i]) == 1)
+	{
+		// for counting the number of elements
+		n++;
+		// for incrementing the array index
+		i++;
+	}
+    //End with storing Integers to array
+
+    
+    //Applying selection sort and finding the execution time
+    double time_taken_by_selection_sort=selection_sort(arr,n);
+
+    printf("Time take by selection sort is :%f",time_taken_by_selection_sort);
+
+	return 0;
 }
--- Implemention/sort.h
+++ Implemention/sort.h
@@ -12,25 +12,44 @@ void print_array(int a[],int size){
         printf("%d ",a[i]);
     }
 }
-//function for bubble sort in ascending order
 
-double bubble_sort(int array[],int size){
-    struct timespec begin, end;
-    clock_t begin= clock_gettime(CLOCK_REALTIME,&begin); 
-/* Execution time for bubble sort begins*/
-    int i,j;
-    for(int i=0;i<size-1;i++){
-        for(int j=0;j<size-i-1;j++){
-            if(array[j]>array[j+1]){
-                swap(&array[j],&array[j+1]);
-            }
-        }
-    }
-    clock_t end=clock_gettime(CLOCK_REALTIME,&end);
-// Execution time for bubble sort ends
-double e_time=(double) (end.tv_nsec - end.tv_nsec)/((double) 100000);
+//function for selection sort in ascending order
+double selection_sort(int arr[],int n){
+    // data type for calculating time
+	clock_t starttime, endtime;
 
+	// variable for calculating total time of execution
+	double totaltime;
+	int i = 0, j, min, index;
 
-print_array(array,size);
-return e_time;
+    // calculating clock when sorting starts..
+	starttime = clock();
+	printf("start time : %f\n", (float)starttime);
+	for (i = 0; i < n - 1; i++) {
+		min = arr[i];
+		for (j = i + 1; j < n; j++) {
+			if (arr[j] < min) {
+				min = arr[j];
+				index = j;
+			}
+		}
+
+		// swapping the smallest number with
+		// the current arr[i]th value
+		int temp = arr[i];
+		arr[i] = min;
+		arr[index] = temp;
+	}
+	// selection sort logic ends here
+
+	// calculating clock when sorting ends
+	endtime = clock();
+    printf("end time of bubble sort is%f\n",endtime);
+
+	totaltime = ((double)(endtime - starttime)) / CLOCKS_PER_SEC;
+    return totaltime;
 }
+//function for selection sort end here.. 
+
+
+
